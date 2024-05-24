5e6b5050c96432d0b07c5663fb6206e934961438
Steelgrou <steelgrou03@gmail.com>
2024-05-24 11:46:29 +0500

    swiper

--- src/components/Header/entrance/Entrance.jsx
+++ src/components/Header/entrance/Entrance.jsx
@@ -1,32 +1,11 @@
-// Import Swiper React components
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import istanbul from '/istanbul.jpg'
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import Slider from "./Slider";
+
 
 export default () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      autoplay={true}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide className='istanbul'><img src={istanbul} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={istanbul} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={istanbul} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={istanbul} alt="" /></SwiperSlide>
-      ...
-    </Swiper>
+    <>
+      <Slider />
+    </>
+
   );
 };
